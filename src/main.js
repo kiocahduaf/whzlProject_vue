c1e40439ca204c1f038492ee1a3e60f49ba797d1
kiocahduaf <1009470059@qq.com>
2019-01-28 09:36:42 +0800

    修改配置文件

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ Vue.prototype.isAuth = isAuth     // 权限方法
 // 保存整站vuex本地储存初始状态
 window.SITE_CONFIG['storeState'] = cloneDeep(store.state)
 
-/* eslint-disable no-new */
+/* eslint-disable */
 new Vue({
   el: '#app',
   router,
